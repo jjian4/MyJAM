962cc626b03b27988fda0e55b7ba64645432cbf2
jjian4 <jjian@umich.edu>
2020-12-21 23:12:20 -0500

    Company search autocomplete working

--- src/components/CompanySelector/CompanySelector.js
+++ src/components/CompanySelector/CompanySelector.js
@@ -1,53 +1,21 @@
 import { useState, useEffect, useCallback, useRef } from "react";
-import { Search, Label } from 'semantic-ui-react'
+import { Search } from 'semantic-ui-react'
+import axios from 'axios';
 
 import "./CompanySelector.scss";
 
-// const source = [
-//     {
-//         "image": "https://s3.amazonaws.com/uifaces/faces/twitter/ricburton/128.jpg",
-//         "title": "Marquardt - Oberbrunner",
-//         "description": "Cross-group value-added conglomeration",
-//         "price": "$85.41"
-//     },
-//     {
-//         "image": "https://s3.amazonaws.com/uifaces/faces/twitter/iqonicd/128.jpg",
-//         "title": "Willms - Bernier",
-//         "description": "Grass-roots intermediate attitude",
-//         "price": "$64.19"
-//     },
-//     {
-//         "image": "https://s3.amazonaws.com/uifaces/faces/twitter/BroumiYoussef/128.jpg",
-//         "title": "Dare, Kub and Hills",
-//         "description": "User-centric asynchronous interface",
-//         "price": "$85.08"
-//     },
-//     {
-//         "image": "https://s3.amazonaws.com/uifaces/faces/twitter/manigm/128.jpg",
-//         "title": "Hills - Flatley",
-//         "description": "Visionary bi-directional capability",
-//         "price": "$81.30"
-//     },
-//     {
-//         "image": "https://s3.amazonaws.com/uifaces/faces/twitter/flexrs/128.jpg",
-//         "title": "O'Connell, Zulauf and Douglas",
-//         "description": "Self-enabling next generation standardization",
-//         "price": "$0.89"
-//     }
-// ];
 
-const source2 = [
-    {
-        'name': 'Amazon',
-        'description': 'hello'
-    },
-    {
-        'name': 'Facebook',
-        'description': 'hellooo'
-    }
-]
-
-const resultRenderer = ({ title }) => <Label content={title} />
+const dropdownEntry = ({ title, domain, logo }) => (
+    <div className='dropdownEntry'>
+        <div className='entryLeft'>
+            <img className='companyLogo' src={logo} alt='logo' />
+            <span className='companyName'>{title}</span>
+        </div>
+        <div className='companyDomain'>
+            {domain}
+        </div>
+    </div>
+)
 
 function CompanySelector() {
     const [loading, setLoading] = useState(false);
@@ -61,18 +29,17 @@ function CompanySelector() {
         setLoading(true)
         setValue(data.value)
 
-        timeoutRef.current = setTimeout(() => {
+        timeoutRef.current = setTimeout(async () => {
             if (data.value.length === 0) {
                 setLoading(false)
                 setResults([])
-                setValue('')
                 return
             }
 
-            //todo
-            const results = JSON.parse(JSON.stringify(source2));
+            const response = await axios.get(`https://autocomplete.clearbit.com/v1/companies/suggest?query=${data.value}`)
+            const results = response.data
 
-            // Change 'name' key to 'title'
+            // Change 'name' key to 'title' due to <Search/> prop requirements
             results.forEach(item => {
                 Object.defineProperty(item, 'title',
                     Object.getOwnPropertyDescriptor(item, 'name'));
@@ -97,7 +64,7 @@ function CompanySelector() {
                 loading={loading}
                 onResultSelect={(e, data) => setValue(data.result.title)}
                 onSearchChange={handleSearchChange}
-                resultRenderer={resultRenderer}
+                resultRenderer={dropdownEntry}
                 results={results}
                 value={value}
             />
