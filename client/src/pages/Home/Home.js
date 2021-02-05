89d019b3e0e533815b3c58b4801d33dcdc3059be
jjian4 <jjian@umich.edu>
2021-02-05 18:01:23 -0500

    Downloaded logo font

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,12 +10,17 @@ function Home() {
   return (
     <div className="Home">
       <div className="content">
-        <div className="appName">
-          {user ? `Hi ${user.givenName || user.displayName}!` : WEBSITE_NAME}
-        </div>
+        <div className="appName">{WEBSITE_NAME}</div>
 
         {/* Explicitly checking for false because null means unknown if logged in */}
-        {user === false && <GoogleOauthButton />}
+        {user === false && (
+          <div>
+            <div className="loginPrompt">Log in or make an account!</div>
+            <GoogleOauthButton />
+          </div>
+        )}
+
+        {user && <div>Hi {user.givenName ?? user.displayName}!</div>}
       </div>
     </div>
   );
